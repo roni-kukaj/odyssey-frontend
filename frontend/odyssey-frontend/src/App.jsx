d461e182780642a14977435b87b81108646d6e39
dinapirana <diinapirana@gmail.com>
2024-05-11 16:27:56 +0200

    ODY-44: Created ContactUs page

--- frontend/odyssey-frontend/src/App.jsx
+++ frontend/odyssey-frontend/src/App.jsx
@@ -2,14 +2,14 @@ import "./Components/LoginSignup/LoginSignup.css"
 import LoginSignup from "./Components/LoginSignup/LoginSignup.jsx"
 import Homepage from "./Components/Homepage/Homepage.jsx"
 import Aboutus from "./Components/AboutUs/Aboutus.jsx"
-
+import ContactUs from "./Components/ContactUs/ContactUs.jsx"
 
 function App() {
   return (
     <div>
         {/* <LoginSignup/> */}
         {/* <Homepage /> */}
-            <Aboutus />
+            {<ContactUs />}
     </div>
 
   )

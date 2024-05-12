f06735ea7c1f7210f8d5898ac22931fcbb95ed77
dinapirana <diinapirana@gmail.com>
2024-05-12 02:56:49 +0200

    ODY-49: Created the React Router

--- frontend/odyssey-frontend/src/App.jsx
+++ frontend/odyssey-frontend/src/App.jsx
@@ -1,20 +1,27 @@
-import "./Components/LoginSignup/LoginSignup.css"
-import LoginSignup from "./Components/LoginSignup/LoginSignup.jsx"
-import Homepage from "./Components/Homepage/Homepage.jsx"
-import Aboutus from "./Components/AboutUs/Aboutus.jsx"
-import ContactUs from "./Components/ContactUs/ContactUs.jsx"
-import News from "./Components/News/News.jsx"
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./Components/LoginSignup/LoginSignup.css";
+import LoginSignup from "./Components/LoginSignup/LoginSignup.jsx";
+import Homepage from "./Components/Homepage/Homepage.jsx";
+import Aboutus from "./Components/AboutUs/Aboutus.jsx";
+import ContactUs from "./Components/ContactUs/ContactUs.jsx";
+import News from "./Components/News/News.jsx";
 
 function App() {
   return (
-    <div>
-        {/* <LoginSignup/> */}
-        {/* <Homepage /> */}
-{/*         <Aboutus /> */}
-{/*         {<ContactUs />} */}
-        <News />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+          <Route path="/aboutus" element={<Aboutus />} />
+          <Route path="/contactus" element={<ContactUs />} />
+          <Route path="/loginsignup" element={<LoginSignup />} />
+          <Route path="/news" element={<News />} />
 
-  )
-  }
-export default App
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
